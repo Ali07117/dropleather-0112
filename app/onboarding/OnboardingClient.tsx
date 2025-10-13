5cd18c4e5578b66f69057a89971ab47a413c68e3
dropleather <bills@dropleather.com>
2025-10-13 10:52:43 +0200

    Update OnboardingClient.tsx

--- app/onboarding/OnboardingClient.tsx
+++ app/onboarding/OnboardingClient.tsx
@@ -697,7 +697,7 @@ const OnboardingPage: React.FC = () => {
                     black
                   `
               }}
-              className={cn("px-4 py-2 mt-0 gap-2 transition-colors duration-300 relative overflow-hidden font-sora font-medium text-xs rounded-lg",
+              className={cn("px-4 py-2 mt-0 gap-2 transition-colors duration-300 relative overflow-hidden font-sora font-medium text-[14px] rounded-lg",
                 formData.theme === 'dark' 
                   ? "!text-black" 
                   : "!text-white",
