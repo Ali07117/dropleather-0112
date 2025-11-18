5ccdb31eab89b3b545bfdd33d942b2302efc4655
Oussama <bills@dropleather.com>
2025-11-18 16:06:26 +0100

    Image Issue

--- components/products/ProductsShowcaseClient.tsx
+++ components/products/ProductsShowcaseClient.tsx
@@ -327,7 +327,7 @@ export function ProductsShowcaseClient() {
           {/* Product Grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {filteredProducts.map((product) => (
-              <Card key={product.id} className="overflow-hidden group">
+              <Card key={product.id} className="overflow-hidden group shadow-none border border-[#E3E3E3]">
                 <CardContent className="p-4">
                   {/* Product Image */}
                   <div className="relative aspect-[2/3] overflow-hidden rounded-lg mb-3">
