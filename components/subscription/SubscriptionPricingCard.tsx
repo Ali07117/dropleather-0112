804f1fd6cb2dd8fff09d1068f59aae4465d40977
dropleather <bills@dropleather.com>
2025-11-28 13:22:00 +0100

    Update SubscriptionPricingCard.tsx

--- components/subscription/SubscriptionPricingCard.tsx
+++ components/subscription/SubscriptionPricingCard.tsx
@@ -237,7 +237,7 @@ const SubscriptionPricingCard: React.FC<SubscriptionPricingCardProps> = ({ billi
                   fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <g weight="bold">
+                  <g>
                     <path d="M204,64V168a12,12,0,0,1-24,0V93L72.49,200.49a12,12,0,0,1-17-17L163,76H88a12,12,0,0,1,0-24H192A12,12,0,0,1,204,64Z"></path>
                   </g>
                 </svg>
